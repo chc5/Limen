913b46c588bb623c6ede44cff5271c2b50314731
chc5 <chc9511@gmail.com>
2018-08-30 10:58:27 -0400

    Added information on the main page.

--- Frontend/src/containers/time_series_list.js
+++ Frontend/src/containers/time_series_list.js
@@ -6,7 +6,7 @@ import Summary from './summary';
 import TimeSeriesButtonGroup from './time_series_button_group';
 import GraphTypeSelectionBar from './graph_type_selection_bar';
 import Loading, { SMALL_SIZE, LARGE_SIZE} from '../components/loading';
-import { LOADING, ERROR } from '../actions/index';
+import { LOADING, ERROR, MAIN } from '../actions/index';
 
 export const META_DATA = "Meta Data";
 
@@ -44,12 +44,37 @@ class TimeSeriesList extends Component{
               : (<Loading size={SMALL_SIZE}/>)
           : ("")
         }
-        {
-          this.props.display === ERROR
+        { this.props.display === ERROR
           ? (
               <div>
                 <h4>Error has occurred. I apologize for your inconvenience.</h4>
                 <h4>Please enter the correct ticker symbol. Thank you!</h4>
+              </div>
+            )
+          : ("")
+        }
+        { this.props.display === MAIN
+          ? (
+              <div className="jumbotron">
+                <h1 className="display-4">Limen</h1>
+                <p className="lead">
+                  An online stock prices analysis website <br />
+                  By Chieh-Huang Chen
+                </p>
+                <hr className="my-4" />
+                <p>This site uses machine learning to analyze trends in stock prices.</p>
+                <div className="lead">
+                  <div>
+                    The following libraries/frameworks have been used to create this website:
+                  </div>
+                  <ul>
+                    <li>React.js / Redux</li>
+                    <li>Django</li>
+                    <li>scikit-learn</li>
+                    <li>Plotly.js</li>
+                    <li>Bootstrap</li>
+                  </ul>
+                </div>
               </div>
             )
           : ("")
